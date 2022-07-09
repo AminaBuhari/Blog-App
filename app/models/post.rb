a36d23bf6cb321ca71675d972059613ecf7af944
buhariaminat <buhariaminat@yahoo.com>
2022-07-09 17:36:55 +0100

    Update method

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,11 +2,14 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :likes
+  after_save :update_post_counter
 
-  def update_posts_counter
+  private
+ 
+  def update_post_counter
     user.increment!(:postscounter)
   end
-
+  
   def recent_comments
     comments.limit(5).order(created_at: :desc)
   end
