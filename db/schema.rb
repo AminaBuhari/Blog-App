eb3a1c6256aef383ab63c4545fbea2d8cb89bfd4
buhariaminat <buhariaminat@yahoo.com>
2022-07-09 15:04:45 +0100

    update foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_09_112249) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_09_135821) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.integer "post_id"
-    t.integer "author_id"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
-    t.integer "post_id"
-    t.integer "author_id"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -35,7 +39,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_09_112249) do
     t.text "text"
     t.integer "comments_counter"
     t.integer "likes_counter"
-    t.integer "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
@@ -52,5 +55,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_09_112249) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
 end
