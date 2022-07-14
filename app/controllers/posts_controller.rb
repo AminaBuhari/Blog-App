a648d4d1682f1e368946264e701195c8d33724bb
buhariaminat <buhariaminat@yahoo.com>
2022-07-14 11:49:02 +0100

    Add post controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,11 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts.includes(:comments)
+   end
 
-  def show; end
+  def show
+    @post = Post.find(params[:id])
+    @comments = Comment.where(post_id: params[:id]).where(author_id: params[:user_id])
+   end
 end
