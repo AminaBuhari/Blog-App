e324af039ce5726f276125904a58293e6ea0de10
buhariaminat <buhariaminat@yahoo.com>
2022-07-14 11:51:35 +0100

    fix linters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,10 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.includes(:comments)
-   end
+  end
 
   def show
     @post = Post.find(params[:id])
     @comments = Comment.where(post_id: params[:id]).where(author_id: params[:user_id])
-   end
+  end
 end
