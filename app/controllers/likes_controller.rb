254e077229e4492ec41ef08c9939b1218323ec93
buhariaminat <buhariaminat@yahoo.com>
2022-07-16 20:44:40 +0100

    fix linters

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,22 +1,20 @@
 class LikesController < ApplicationController
-
   def create
     set_like
     @like = current_user.likes.new(author_id: current_user.id, post_id: params[:post_id])
-  
+
     respond_to do |format|
       if @like.save
         format.html do
-        redirect_to user_post_path(@like.author_id, @like.post_id)
+          redirect_to user_post_path(@like.author_id, @like.post_id)
+        end
       end
     end
-   end
   end
-  
+
   private
-  
+
   def set_like
     @like = Like.new
   end
-
 end
