084c487c651b7ae7877079389205f437094f0867
Roberto Andres Condezo Monge <ra.condezomonge@gmail.com>
2022-07-21 13:01:37 -0500

    Update post_spec test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,46 +1,48 @@
 require 'rails_helper'
 
-# RSpec.describe 'Posts', type: :request do
-#   describe 'GET /index' do
-#     it 'returns http success' do
-#       get '/posts/index'
-#       expect(response).to have_http_status(:success)
-#     end
-#   end
-# end
+RSpec.describe 'Posts controller', type: :request do
+  before(:all) do
+    @author = User.create(name: 'Tom',
+                          photo: 'https://images513-2c110a25bd8c&q=80',
+                          bio: 'I am a teacher from Mexico',
+                          post_counter: 0)
 
-RSpec.describe 'Test index action', type: :request do
-  before(:example) do
-    @user = User.create!(name: 'Tom', photo: 'https://i.ibb.co/n6R1Zh1/At-hospital.jpg', bio: 'Hospital.')
-    get user_posts_path(@user)
-  end
-  it 'is a success' do
-    expect(response).to have_http_status(:ok)
-  end
-  it 'renders template' do
-    expect(response).to render_template('index')
+    @post = Post.create(user: @author,
+                        title: 'post 1',
+                        text: 'text 1',
+                        comments_counter: 0,
+                        likes_counter: 0)
   end
 
-  it 'should display the correct placeholder text' do
-    expect(response.body).to include('Here is a list of all posts by a user')
-  end
-end
+  context 'GET #index' do
+    before { get user_posts_path(@author.id) }
 
-RSpec.describe 'Test index action' do
-  before(:example) do
-    @user = User.create!(name: 'Tom', photo: 'https://unsplash.com/photos/F_0BxGuVvo', bio: 'Teacher from Mexico.')
-    @post = @user.posts.create!(title: 'The ephicacy of things', text: 'Wow the ephicacy of things',
-                                comments_counter: 0, likes_counter: 0)
-    get user_post_path(@user, @post)
-  end
-  it 'is a success' do
-    expect(response).to have_http_status(:ok)
-  end
-  it 'renders template' do
-    expect(response).to render_template(:show)
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'index' template" do
+      expect(response).to render_template('index')
+    end
+
+    it 'the response body includes correct placeholder text.' do
+      expect(response.body).to include('text 1')
+    end
   end
 
-  it 'should display the correct placeholder text' do
-    expect(response.body).to include('Here is a post by username')
+  context 'GET #show' do
+    before { get user_post_path(@author.id, @post.id) }
+
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'show' template" do
+      expect(response).to render_template('show')
+    end
+
+    it 'the response body includes correct placeholder text.' do
+      expect(response.body).to include('post 1')
+    end
   end
 end
